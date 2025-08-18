d8ed71da20b312fca88b939837076aa7d319b79a
jtb75 <joe.buhr@gmail.com>
2025-08-18 07:32:45 -0500

    remove hardcoded key

--- tf/backend.tf
+++ tf/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     # Backend configuration will be provided during init
     # via -backend-config flags or backend config file
+    # Key is dynamically set in GitHub Actions:
+    # - dev: /dev/terraform.tfstate
+    # - prod: /prod/terraform.tfstate
     encrypt = true
-    key = "state.tfstate"
   }
 }
